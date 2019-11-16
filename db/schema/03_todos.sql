aafb0a94d7e33fb274108807c26af42f81755b45
Caronise <lucas.betancourt@hotmail.com>
2019-11-16 15:39:31 +0000

    Created the users, categories and todos schemas and seed
    s for for the tables

---
+++ db/schema/03_todos.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS todos CASCADE;
+
+CREATE TABLE todos (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+  description TEXT NOT NULL,
+  date_created DATE NOT NULL,
+  date_due DATE NOT NULL,
+  priority SMALLINT NOT NULL,
+  completed BOOLEAN NOT NULL
+);
